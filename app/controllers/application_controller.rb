97c6753fe692044a16d47724065fc52efe2d2a8b
Fladson Gomes <fladsonthiago@gmail.com>
2022-05-27 17:06:15 +0200

    Add require_login callback and helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  def current_user
+    if session[:profile_uuid]
+      @profile_name = session[:profile_name]
+    end
+  end
+
+  private
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:error] = I18n.t "authorization.access_denied"
+      redirect_to root_path
+    end
+  end
 end
