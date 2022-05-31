dbb596c6ac616f949dfd988f7c7635df5358fa6a
Fladson Gomes <fladsonthiago@gmail.com>
2022-05-31 12:17:27 +0200

    Add Account#leagues_playing method

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,4 +2,7 @@ class Account < ApplicationRecord
   has_one :oauth_refresh_token, dependent: :destroy
   has_one :league_account, dependent: :destroy
   has_many :characters, dependent: :destroy
+  def leagues_playing
+    characters.distinct.pluck(:league)
+  end
 end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,4 +1,9 @@
 require "test_helper"
 
 class AccountTest < ActiveSupport::TestCase
+  test "leagues playing" do
+    account = accounts(:one)
+    expected_leagues = ["Standard", "Wealthclast", "Wealthclast SSF"].sort
+    assert_equal account.leagues_playing.sort, expected_leagues
+  end
 end
