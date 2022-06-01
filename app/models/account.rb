d4b8df03f9c7f1441d2e4813237ef96ff9713a2f
Fladson Gomes <fladsonthiago@gmail.com>
2022-06-01 22:17:27 +0200

    Add scopes to Account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,8 +1,20 @@
 class Account < ApplicationRecord
   has_one :oauth_refresh_token, dependent: :destroy
-  has_one :league_account, dependent: :destroy
+  has_many :league_accounts, dependent: :destroy
   has_many :characters, dependent: :destroy
-  def leagues_playing
+
+  delegate :refresh_token, to: :oauth_refresh_token
+
+  def leagues_playing_from_chars
     characters.distinct.pluck(:league)
+  end
+
+  def private_leagues_from_chars
+    leagues_playing_from_chars - League.not_private.distinct.pluck(:name)
+  end
+
+  # Does the account has any private league that is not saved
+  def any_private_league_not_saved?
+    private_leagues_from_chars - League.where(private: true).distinct.pluck(:name)
   end
 end
