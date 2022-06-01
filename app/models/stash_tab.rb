b6edb44a06a9f01fc682f636b453acc0ad8c5c1a
Fladson Gomes <fladsonthiago@gmail.com>
2022-06-01 22:17:05 +0200

    Add StashTab::VALID_TYPES

--- app/models/stash_tab.rb
+++ app/models/stash_tab.rb
@@ -1,4 +1,6 @@
 class StashTab < ApplicationRecord
   self.inheritance_column = nil # allow type as column name
   belongs_to :league_account
+
+  VALID_TYPES = %(NormalStash PremiumStash CurrencyStash Folder)
 end
