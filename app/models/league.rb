be06af90f88834ce2b1a84ff6bba3db902095dd3
Fladson Gomes <fladsonthiago@gmail.com>
2022-06-01 22:16:34 +0200

    Add League scopes

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,3 +1,7 @@
 class League < ApplicationRecord
   has_many :items, dependent: :restrict_with_error
+
+  scope :not_private, -> { where(private: false) }
+  scope :permanent, -> { where(ends_at: nil) }
+  scope :temporary, -> { where.not(ends_at: nil) }
 end
