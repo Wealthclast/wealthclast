18573a3e358f0124cb44804cbe9b797c1737a72f
Fladson Gomes <fladsonthiago@gmail.com>
2022-06-01 22:16:50 +0200

    Add LeagueAccount temporary scope

--- app/models/league_account.rb
+++ app/models/league_account.rb
@@ -2,4 +2,6 @@ class LeagueAccount < ApplicationRecord
   belongs_to :account
   belongs_to :league
   has_many :item_snapshots, dependent: :destroy
+
+  scope :temporary, -> { joins(:league).where.not(league: {ends_at: nil}) }
 end
